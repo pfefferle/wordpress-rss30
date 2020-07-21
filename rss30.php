2dd0a159218e34f93cd5008647a81061f2174c0d
Matthias Pfefferle <pfefferle@gmail.com>
2020-07-21 22:12:05 +0200

    initial

---
+++ rss30.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Plugin Name: RSS 3.0
+ * Plugin URI: https://github.com/pfefferle/wordpress-rss30
+ * Description:
+ * Author: Matthias Pfefferle
+ * Author URI: https://notiz.blog
+ * Version: 1.0.0
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: rss30
+ */
+
+register_activation_hook( __FILE__, 'rss30_flush_rewrite_rules' );
+register_deactivation_hook( __FILE__, 'rss30_flush_rewrite_rules' );
+
+/**
+ * Init function
+ */
+function rss30_init() {
+	add_filter( 'feed_content_type', 'rss30_feed_content_type', 10, 2 );
+	add_feed( 'rss3', 'do_feed_rss30' );
+	add_action( 'do_feed_rss30', 'do_feed_rss30', 10, 1 );
+}
+add_action( 'init', 'rss30_init' );
+
+/**
+ * Adds "rss3" content-type
+ *
+ * @param string $content_type the default content-type
+ * @param string $type the feed-type
+ *
+ * @return string the as1 content-type
+ */
+function rss30_feed_content_type( $content_type, $type ) {
+	if ( 'rss3' === $type ) {
+		return 'text/plain';
+	}
+
+	return $content_type;
+}
+
+/**
+ * Adds an rss30 json feed
+ */
+function do_feed_rss30( $for_comments ) {
+	if ( ! $for_comments ) {
+		// load post template
+		load_template( dirname( __FILE__ ) . '/templates/feed-rss30.php' );
+	}
+}
+
+/**
+ * Reset rewrite rules
+ */
+function rss30_flush_rewrite_rules() {
+	global $wp_rewrite;
+	$wp_rewrite->flush_rules();
+}
---
+++ templates/feed-rss30.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * RSS2 Feed Template for displaying RSS2 Posts feed.
+ *
+ * @package WordPress
+ */
+
+header( 'Content-Type: ' . feed_content_type( 'rss3' ) . '; charset=' . get_option( 'blog_charset' ), true );
+
+do_action( 'rss_tag_pre', 'rss3' );
+?>
+title: <?php wp_title_rss(); ?><?php echo PHP_EOL; ?>
+description: <?php bloginfo_rss( 'description' ); ?><?php echo PHP_EOL; ?>
+link: <?php echo site_url( '/' ); ?><?php echo PHP_EOL; ?>
+language: <?php bloginfo_rss( 'language' ); ?><?php echo PHP_EOL; ?>
+creator: <?php echo site_url( '/' ); ?><?php echo PHP_EOL; ?>
+guid: <?php echo site_url( '/' ); ?><?php echo PHP_EOL; ?>
+generator: http://www.movabletype.org/?v=2.64<?php echo PHP_EOL; ?>
+
+<?php
+/**
+ * Fires at the end of the RSS3 Feed Header.
+ */
+do_action( 'rss3_head' );
+
+while ( have_posts() ) :
+	the_post();
+	?>
+title: <?php the_title_rss(); ?><?php echo PHP_EOL; ?>
+link: <?php the_permalink_rss(); ?><?php echo PHP_EOL; ?>
+description: <?php the_excerpt_rss(); ?><?php echo PHP_EOL; ?>
+date: <?php echo mysql2date( 'c', get_post_time( 'Y-m-d H:i:s', true ), false ); ?><?php echo PHP_EOL; ?>
+creator: <?php the_author(); ?><?php echo PHP_EOL; ?>
+<?php echo PHP_EOL; ?>
+<?php
+endwhile;
