122779ef656db52e9a2b3f58fa9f68edae0eeba0
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-07-09 13:37:07 +0200

    fix encoding

--- templates/feed-rss30.php
+++ templates/feed-rss30.php
@@ -10,7 +10,7 @@ header( 'Content-Type: ' . feed_content_type( 'rss3' ) . '; charset=' . get_opti
 do_action( 'rss_tag_pre', 'rss3' );
 ?>
 title: <?php wp_title_rss(); ?><?php echo PHP_EOL; ?>
-description: <?php wp_specialchars_decode( wp_strip_all_tags( bloginfo_rss( 'description' ) ) ); ?><?php echo PHP_EOL; ?>
+description: <?php get_bloginfo_rss( 'description' ); ?><?php echo PHP_EOL; ?>
 link: <?php echo site_url( '/' ); ?><?php echo PHP_EOL; ?>
 language: <?php bloginfo_rss( 'language' ); ?><?php echo PHP_EOL; ?>
 creator: <?php echo site_url( '/' ); ?><?php echo PHP_EOL; ?>
@@ -26,9 +26,9 @@ do_action( 'rss3_head' );
 while ( have_posts() ) :
 	the_post();
 	?>
-title: <?php wp_specialchars_decode( the_title_rss() ); ?><?php echo PHP_EOL; ?>
+title: <?php the_title_rss(); ?><?php echo PHP_EOL; ?>
 link: <?php the_permalink_rss(); ?><?php echo PHP_EOL; ?>
-description: <?php wp_specialchars_decode( wp_strip_all_tags( the_excerpt_rss() ) ); ?><?php echo PHP_EOL; ?>
+description: <?php echo html_entity_decode( get_the_excerpt() ); ?><?php echo PHP_EOL; ?>
 date: <?php echo mysql2date( 'c', get_post_time( 'Y-m-d H:i:s', true ), false ); ?><?php echo PHP_EOL; ?>
 creator: <?php the_author(); ?><?php echo PHP_EOL; ?>
 <?php echo PHP_EOL; ?>
